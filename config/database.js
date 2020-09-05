5775770269fefc8ba1c908e18e36368f2915a921
harkamalsi <harkamal_guraya@hotmail.com>
2020-09-05 12:32:15 +0200

    Changed deployment settings

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       settings: {
         //uri: env("DATABASE_URI"),
         uri: "${process.env.DATABASE_URI}",
-        database: "${process.env.DATABASE_NAME}",
+        //database: "${process.env.DATABASE_NAME}",
         /* host: env('DATABASE_HOST', '127.0.0.1'),
         srv: env.bool('DATABASE_SRV', false),
         port: env.int('DATABASE_PORT', 27017),
