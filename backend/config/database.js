e7b64cfe74d4e830b3a6d3c3509cd3a00e0f2a45
harkamalsi <harkamal_guraya@hotmail.com>
2020-09-05 10:27:43 +0200

    Added support for env vars

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        uri: env("DATABASE_URI"),
+        uri: env("DATABASE_URI") || "${process.env.DATABASE_URI}",
         /* host: env('DATABASE_HOST', '127.0.0.1'),
         srv: env.bool('DATABASE_SRV', false),
         port: env.int('DATABASE_PORT', 27017),
